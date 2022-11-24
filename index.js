e1021b38230925575ecab5e6c8faf8afd8bd3d46
the mordhon <romdhonm12@gmail.com>
2022-11-24 11:23:48 +0700

    response method result

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const express = require('express');
 const app = express();
 
 app.get('/product', (req, res, next) => {
+	const data = {
+		"id": 5,
+		"name": "The Mordhon"
+	}
 	console.log('url', req.url);
 	console.log('method', req.method);
+	res.json(data);
+	next();
 })
 
 // app.use()
