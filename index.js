d3085ad6e53a91033e0b3543f67eea1abf5e53e2
the mordhon <romdhonm12@gmail.com>
2022-11-24 14:02:10 +0700

    handel CORS policy

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const app = express();
 
 const productRoutes = require('./src/routes/products')
 
-app.use(productRoutes)
+/* Handle CORS */
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, PATCH, DELETE, OPTIONS');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	next();
+})
+
+
+app.use('/v1', productRoutes);
 
 app.listen(4000);
--- src/routes/products.js
+++ src/routes/products.js
@@ -4,8 +4,19 @@ const router = express();
 
 const productsController = require('../controllers/products');
 
+// READ -> GET
 router.get('/product', productsController.createProduct)
 router.get('/products', productsController.getAllProduct)
+
 /* Other method can be seen in postman cause browser only support for GET method */
+
+// CREATE -> POST
+router.post('/post/products', productsController.getAllProduct)
+
+// UPDATE -> PUT
+router.put('/put/products', productsController.getAllProduct)
+
+// DELETE -> DELETE
+router.delete('/delete/products', productsController.getAllProduct)
 
 module.exports = router;
